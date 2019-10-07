3ce136491762decff05e8fb1103420dec0f340ca
nmemisevic1 <nmemisevic1@etf.unsa.ba>
2019-10-07 18:14:40 +0200

    Nova izmjena

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,3 @@
-// Your web app's Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyAgbF4zsBgfSGYfDMlxzXKbkr1J8SdQPxw",
-  authDomain: "nmemisevic1portfolio.firebaseapp.com",
-  databaseURL: "https://nmemisevic1portfolio.firebaseio.com",
-  projectId: "nmemisevic1portfolio",
-  storageBucket: "nmemisevic1portfolio.appspot.com",
-  messagingSenderId: "247580151493",
-  appId: "1:247580151493:web:58d429adf3331a27592aac"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Reference ips collection
-var visits = firebase.database.ref('Visits');
-
-
 $(function() {
 
   var isMobile;
@@ -185,6 +168,21 @@ $(function() {
   })
 
 });
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAgbF4zsBgfSGYfDMlxzXKbkr1J8SdQPxw",
+  authDomain: "nmemisevic1portfolio.firebaseapp.com",
+  databaseURL: "https://nmemisevic1portfolio.firebaseio.com",
+  projectId: "nmemisevic1portfolio",
+  storageBucket: "nmemisevic1portfolio.appspot.com",
+  messagingSenderId: "247580151493",
+  appId: "1:247580151493:web:58d429adf3331a27592aac"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Reference ips collection
+var visits = firebase.database().ref('Visits');
 function registerVisit(){
   var ipAddress;
   /*$.getJSON("http://jsonip.com?callback=?", function (data) {
