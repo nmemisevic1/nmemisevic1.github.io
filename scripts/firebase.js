a8c7354efbc9b1ddfd9d106e36827b3bc244e250
nmemisevic1 <nmemisevic1@etf.unsa.ba>
2019-10-09 16:27:52 +0200

    Nova izmjena

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,11 +1,1 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyAgbF4zsBgfSGYfDMlxzXKbkr1J8SdQPxw",
-    authDomain: "nmemisevic1portfolio.firebaseapp.com",
-    databaseURL: "https://nmemisevic1portfolio.firebaseio.com",
-    projectId: "nmemisevic1portfolio",
-    storageBucket: "nmemisevic1portfolio.appspot.com",
-    messagingSenderId: "247580151493",
-    appId: "1:247580151493:web:58d429adf3331a27592aac"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+var firebaseConfig={apiKey:"AIzaSyAgbF4zsBgfSGYfDMlxzXKbkr1J8SdQPxw",authDomain: "nmemisevic1portfolio.firebaseapp.com",databaseURL: "https://nmemisevic1portfolio.firebaseio.com",projectId: "nmemisevic1portfolio",storageBucket: "nmemisevic1portfolio.appspot.com",messagingSenderId: "247580151493",appId: "1:247580151493:web:58d429adf3331a27592aac"};firebase.initializeApp(firebaseConfig);
