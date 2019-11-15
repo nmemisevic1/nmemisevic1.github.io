f2a41251d719c9c7eb0c558591316efc16ab33cb
Nedim Memišević <nmemisevic1@etf.unsa.ba>
2019-11-15 16:25:27 +0100

    Date

--- scripts/firebaseadder.js
+++ scripts/firebaseadder.js
@@ -1,26 +1,33 @@
 var visits = firebase.database().ref('Visits');
 
 
-function registerVisit(){
-  /*$.getJSON("http://jsonip.com?callback=?", function (data) {
-        this.ipAddress = data.ip;
+function registerVisit() {
+    /*$.getJSON("http://jsonip.com?callback=?", function (data) {
+          this.ipAddress = data.ip;
 
-  });
-  alert("Your ip address: " + ipAddress);*/
-  $.getJSON('https://ipapi.co/json/', function(data) {
-  var JSONObject = JSON.parse(JSON.stringify(data,null,2));
-  //console.log(JSON.stringify(data, null, 2));
-  //console.log(JSONObject.ip);
-  //console.log(JSONObject.city);
-  //console.log(JSONObject.country_name);
-  saveVisit(JSONObject.ip,JSONObject.city,JSONObject.country_name);
-});
+    });
+    alert("Your ip address: " + ipAddress);*/
+    $.getJSON('https://ipapi.co/json/', function(data) {
+        var JSONObject = JSON.parse(JSON.stringify(data, null, 2));
+        //console.log(JSON.stringify(data, null, 2));
+        //console.log(JSONObject.ip);
+        //console.log(JSONObject.city);
+        //console.log(JSONObject.country_name);
+        var datum = new Date();
+        var vrijeme = "";
+        vrijeme += datum.getDate() + "/" + (datum.getMonth() + 1) + "/" + datum.getFullYear();
+        vrijeme += " " + datum.getHours() + ":" + datum.getMinutes();
+        saveVisit(JSONObject.ip, JSONObject.city, JSONObject.country_name, vrijeme);
+        //console.log(vrijeme);
+    });
 }
-function saveVisit(IPAddress,City,Country){
-  var newVisit = visits.push();
-  newVisit.set({
-    IPAddress: IPAddress,
-    Country: Country,
-    City: City
-  })
+
+function saveVisit(IPAddress, City, Country, Time) {
+    var newVisit = visits.push();
+    newVisit.set({
+        IPAddress: IPAddress,
+        Country: Country,
+        City: City,
+        Time: Time
+    })
 }
